4c8c5918291a1eede1f193af43f3db2a5ef8400a
VladimirYurenko1993 <vladimiryurenko1993@gmail.com>
2022-03-23 08:54:08 +0200

    07-branching

--- 07-branching.js
+++ 07-branching.js
@@ -1,2 +1,45 @@
 // Оператор ветвления if
 
+// if (0) {
+//     //тело
+//     console.log('12345');
+// }
+// console.log(678910);
+
+
+// if (5 > 30) {
+//     console.log('x > y');
+// } else {
+//     console.log('x < y');
+// }
+
+
+// const salary = 500
+// if (salary <= 500) {
+//     console.log('Level 1');
+// } else if (salary > 500 && salary < 1500) {
+//     console.log('Level 2');
+// } else if (salary > 1500 && salary < 3000) {
+//     console.log('Level 3');
+// } else {
+//     console.log('Level 4');
+// }
+
+
+// Тернарный оператор
+
+const balance = 1000
+// let message
+
+// if (balance => 0) {
+//     message = 'Positive balace'
+// } else {
+//     message = 'Negative balance'
+// }
+
+const message = balance >=0 ? 'Positive balace' : 'Negative balance'
+// console.log(message);
+
+
+// Блочная область видимости переменных
+
