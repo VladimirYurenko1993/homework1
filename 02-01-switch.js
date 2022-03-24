c7f7abb9cdd5852a5c893d9c158de15027ca7711
VladimirYurenko1993 <vladimiryurenko1993@gmail.com>
2022-03-24 17:41:32 +0200

    02-01-swich

---
+++ 02-01-switch.js
@@ -0,0 +1,48 @@
+/* Напиши скрипт выбора стоимости отеля по кол-ву звёзд.
+    1 - 20$, 2 - 30$, 3 - 50$, 4 - 70$, 5 - 120$
+    
+Если в переменной stars что-то кроме чисел 1-5, выведи строку: 'Такого кол-ва звёзд нет' */
+
+const stars = 5
+let price
+
+// if (stars === 1) {
+//     price = 20
+// } else if (stars === 2) {
+//     price = 30
+// } else if (stars === 3) {
+//     price = 50
+// } else if (stars === 4) {
+//     price = 70
+// } else if (stars === 5) {
+//     price = 120
+// } else {
+// console.log('Такого кол-ва звёзд нет');
+// }
+
+switch (stars) {
+    case 1: 
+        price = 20
+        break
+    
+    case 2:
+        price = 30
+        break
+    
+    case 3:
+        price = 50
+        break
+    
+    case 4:
+        price = 70
+        break
+
+    case 5:
+        price = 120
+        break
+    
+    default:
+        console.log('Такого кол-ва звёзд нет');
+}
+
+console.log(price);
